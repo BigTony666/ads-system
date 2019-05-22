2eb3ec82dfd49a8515eb8c1cff318f6c56f81130
BigTony <kejiezhang666@gmail.com>
2019-05-22 15:26:33 +0800

    feat: add global config

---
+++ bigtony-ad-service/ad-common/src/main/java/com/bigtony/ad/exception/AdException.java
@@ -0,0 +1,10 @@
+package com.bigtony.ad.exception;
+
+/**
+ * Unified Exception
+ */
+public class AdException extends Exception{
+    public AdException(String message) {
+        super(message);
+    }
+}
